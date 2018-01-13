ae5744017f92008be0b073b6630daccdc5a717d7
reginashay <shaidullina_1999@mail.ru>
2018-01-13 23:49:08 +0300

    done solve()

--- homework02/sudoku.py
+++ homework02/sudoku.py
@@ -114,7 +114,15 @@ def solve(grid):
     >>> solve(grid)
     [['5', '3', '4', '6', '7', '8', '9', '1', '2'], ['6', '7', '2', '1', '9', '5', '3', '4', '8'], ['1', '9', '8', '3', '4', '2', '5', '6', '7'], ['8', '5', '9', '7', '6', '1', '4', '2', '3'], ['4', '2', '6', '8', '5', '3', '7', '9', '1'], ['7', '1', '3', '9', '2', '4', '8', '5', '6'], ['9', '6', '1', '5', '3', '7', '2', '8', '4'], ['2', '8', '7', '4', '1', '9', '6', '3', '5'], ['3', '4', '5', '2', '8', '6', '1', '7', '9']]
     """
-    pass
+    pos = find_empty_positions(grid)
+    if pos is not None:
+        for i in find_possible_values(grid, pos):
+            grid[pos[0]][pos[1]] = i
+            if solve(grid) is not None:
+                return grid
+            grid[pos[0]][pos[1]] = '.'
+    else:
+        return grid
 
 
 def check_solution(solution):
