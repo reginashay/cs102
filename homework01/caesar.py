343b62622004d4f27a511ed433c40798c1a9dea2
reginashay <shaidullina_1999@mail.ru>
2018-01-13 22:55:50 +0300

    Реализована функция encrypt_caesar()

--- homework01/caesar.py
+++ homework01/caesar.py
@@ -1,15 +1,28 @@
 def encrypt_caesar(plaintext):
     """
+    Encrypts plaintext using a Caesar cipher.
+
     >>> encrypt_caesar("PYTHON")
     'SBWKRQ'
     >>> encrypt_caesar("python")
     'sbwkrq'
-    >>> encrypt_caesar("Python3.6")
-    'Sbwkrq3.6'
+    >>> encrypt_caesar("0123456")
+    '0123456'
+    >>> encrypt_caesar("@#^&*${}><~?")
+    '@#^&*${}><~?'
     >>> encrypt_caesar("")
     ''
     """
-    # PUT YOUR CODE HERE
+    ciphertext = ""
+    de = 26
+    i = 3
+    for c in plaintext:
+        if (68 <= ord(c)+i <= 90) or (100 <= ord(c)+i <= 122):
+            ciphertext += chr(ord(c)+i)
+        elif (ord(c) <= 64) or (91 <= ord(c) <= 96) or (123 <= ord(c)):
+            ciphertext += chr(ord(c))
+        else:
+            ciphertext += chr(ord(c)+i-de)
     return ciphertext
 
 
