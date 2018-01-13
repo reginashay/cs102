922d63e1818dc9deed435165301f1d4f10ffdfce
reginashay <shaidullina_1999@mail.ru>
2018-01-13 22:56:35 +0300

    Реализована функция decrypt_caesar()

--- homework01/caesar.py
+++ homework01/caesar.py
@@ -28,14 +28,27 @@ def encrypt_caesar(plaintext):
 
 def decrypt_caesar(ciphertext):
     """
+    Decrypts a ciphertext using a Caesar cipher.
+
     >>> decrypt_caesar("SBWKRQ")
     'PYTHON'
     >>> decrypt_caesar("sbwkrq")
     'python'
-    >>> decrypt_caesar("Sbwkrq3.6")
-    'Python3.6'
+    >>> decrypt_caesar("0123456")
+    '0123456'
+    >>> decrypt_caesar("@#^&*${}><~?")
+    '@#^&*${}><~?'
     >>> decrypt_caesar("")
     ''
     """
-    # PUT YOUR CODE HERE
+    plaintext = ""
+    de = 26
+    i = 3
+    for c in ciphertext:
+        if (65 <= ord(c)-i <= 87) or (97 <= ord(c)-i <= 119):
+            plaintext += chr(ord(c)-i)
+        elif (ord(c) <= 64) or (91 <= ord(c) <= 96) or (123 <= ord(c)):
+            plaintext += chr(ord(c))
+        else:
+            plaintext += chr(ord(c)-i+de)
     return plaintext
