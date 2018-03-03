596a179213f265d1d580443555716f3d473815b1
reginashay <shaidullina_1999@mail.ru>
2018-03-03 23:19:07 +0300

    added rows to database

---
+++ homework06/db.py
@@ -0,0 +1,30 @@
+from sqlalchemy import Column, String, Integer
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+#from scraputils import get_news
+
+
+Base = declarative_base()
+engine = create_engine("sqlite:///news.db")
+session = sessionmaker(bind=engine)
+
+class News(Base):
+    __tablename__ = "news"
+    id = Column(Integer, primary_key=True)
+    title = Column(String)
+    author = Column(String)
+    url = Column(String)
+    comments = Column(Integer)
+    points = Column(Integer)
+    label = Column(String)
+
+Base.metadata.create_all(bind=engine)
+
+
+#def append_news():
+#    s = session()
+#    news = get_news('https://news.ycombinator.com/', n_pages=30)
+#    for post in news:
+#        s.add(News(**post))
+#    s.commit()
